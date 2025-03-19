21cf1af2ca02178745f7355afb0c9e3f6cc6d5ce
lutfubedel <lutfubedel13@gmail.com>
2025-03-19 11:47:49 +0300

    .

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,23 @@
 import logo from "../assets/logoDark.svg"
 import FriendsBar from "./FriendsBar";
 import { motion } from "framer-motion";
+import { getAuth } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+    const auth = getAuth();
+    const navigate = useNavigate();
+    
+    const user = auth.currentUser;
+    console.log(user);
+
+    const handleLogout = () => {
+        console.log("Logging out...");
+        localStorage.removeItem("user");
+        sessionStorage.removeItem("user");
+        navigate("/login");
+      };
+
     return(
         <>
             <motion.div
@@ -12,13 +27,24 @@ const Home = () => {
                 transition={{ duration: 0.2 }}   // 0.5 saniyede geçiş yapıyor
                 className="fixed top-0 left-0 w-full h-screen"
                 >
+               
                 <div className="fixed grid grid-cols-3 bg-[#B3C8CF] text-[#E5E1DA] h-screen w-[calc(100vw-16rem)] left-16 top-0 z-0">
                     <div><FriendsBar /></div> 
                     <div className="w-auto h-auto col-start-2 mt-auto mb-auto text-5xl font-bold">
                         <img src={logo} className="rounded-full opacity-15" />
                         HOME
-                    </div>   
+                    </div>  
+                    <div>
+                    <button 
+                        type="button" 
+                        className="font-semibold text-indigo-600 hover:text-indigo-500"
+                        onClick={handleLogout}
+                    >
+                        Log Out
+                    </button>
+                </div> 
                 </div>
+                
             </motion.div>    
         </>
     );   
