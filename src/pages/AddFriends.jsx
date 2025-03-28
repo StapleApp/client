33abcccd62841f9a72e88be594505016569e326d
ahmetyusufbirdir03 <168556979+ahmetyusufbirdir03@users.noreply.github.com>
2025-03-28 18:24:00 +0300

    add friend page update

--- src/pages/AddFriends.jsx
+++ src/pages/AddFriends.jsx
@@ -7,7 +7,8 @@ import { IoIosSearch, IoMdPersonAdd } from "react-icons/io"; // Arama ikonu
 import profileBackground2_small from "../assets/profileBackground2_small.png";
 import { useAuth } from "../context/AuthContext";
 import { GetUserByFriendshipID } from "../../firebase";
-import toast from 'react-hot-toast';
+import toast from "react-hot-toast";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 const ArkadasEkle = () => {
   const { currentUser, userData } = useAuth();
@@ -18,18 +19,24 @@ const ArkadasEkle = () => {
 
   const handleSearch = () => {
     if (searchId) {
-      setShowProfile(true);
+      // Profil ekranını kapatıyoruz, animasyon sıfırlansın
+      setShowProfile(false);
+
+      // Yeni bir arama yapıldığında 0.1 saniye sonra profil ekranını açıyoruz
+      setTimeout(() => {
+        setShowProfile(true);
+      }, 100); // 0.1 saniye sonra animasyon başlasın
 
       GetUserByFriendshipID(searchId).then((friend) => {
         if (friend) {
-            setFriendData(friend); 
-            console.log("User found:", friend);
+          setFriendData(friend);
+          console.log("User found:", friend);
         } else {
-            setFriendData(null); 
-            toast.error("No user found with this friendshipID.");
-            console.log("No user found with this friendshipID.");
+          setFriendData(null);
+          toast.error("No user found with this friendshipID.");
+          console.log("No user found with this friendshipID.");
         }
-    });
+      });
     }
   };
 
@@ -39,7 +46,7 @@ const ArkadasEkle = () => {
         initial={{ opacity: 0, x: -100 }}
         animate={{ opacity: 1, x: 0 }}
         exit={{ opacity: 0, x: 100 }}
-        transition={{ duration: 0.1 }}
+        transition={{ duration: 1 }}
         className="background fixed top-0 left-0 w-full h-screen bg-[var(--secondary-bg)] text-[var(--secondary-text)]"
       >
         {/* Arama Çubuğu */}
@@ -49,6 +56,7 @@ const ArkadasEkle = () => {
             placeholder="arkadaş ara..."
             value={searchId}
             onChange={(e) => setSearchId(e.target.value)}
+            onKeyDown={(e) => e.key === "Enter" && handleSearch()} // Enter tuşu ile arama yap
             className="w-90 p-2 rounded-lg border-2 border-[var(--secondary-border)] focus:outline-none focus:border-[var(--tertiary-border)] bg-[var(--primary-bg)]"
           />
           <button
@@ -59,99 +67,113 @@ const ArkadasEkle = () => {
           </button>
         </div>
 
-        <div className="relative w-110 h-110 mx-auto bg-[var(--primary-bg)] rounded-lg pt-5">
-          {/* Profil Fotoğrafı ve Kullanıcı Bilgileri */}
-          <div
-            className="flex items-center justify-start relative w-100 h-30 bg-[var(--secondary-bg)] mx-auto pl-10 rounded-lg bg-cover bg-center"
-            style={{ backgroundImage: `url(${profileBackground2_small})` }}
-          >
-            {/* Profil Fotoğrafı Dönen Kart */}
-            <div className="relative w-22 h-22 group">
-              <div className="w-full h-full relative transition-transform duration-700 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
-                {/* Ön Yüz (Profil Fotoğrafı) */}
-                <div className="absolute w-full h-full backface-hidden">
-                  <img
-                    src={pfp}
-                    alt="Profil"
-                    className="w-full h-full rounded-full border-4 border-[var(--tertiary-border)] shadow-lg"
-                  />
-                </div>
+        {/* Animasyon: İlk başta profil gözükmezken gösterilecek */}
+        {!showProfile && (
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+            {addFriendAnim()}
+            <span className="block text-lg font-semibold text-[var(--quaternary-text)]">
+              FIND YOUR FRIENDS
+            </span>
+          </div>
+        )}
 
-                {/* Arka Yüz (Arkadaş Ekle Butonu) */}
-                <div className="absolute w-full h-full flex items-center justify-center bg-[var(--secondary-bg)] rounded-full border-4 border-[var(--tertiary-border)] shadow-lg [transform:rotateY(180deg)] backface-hidden">
-                  <button
-                    className="text-white text-xl"
-                    onClick={handleAddFriend}
-                  >
-                    <IoMdPersonAdd />
-                  </button>
+        {showProfile && (
+          <motion.div
+            initial={{ opacity: 0, y: -50 }} // Başlangıçta şeffaf ve yukarıda
+            animate={{ opacity: 1, y: 0 }} // Görünür hale gelince, yukarıdan normal pozisyona gelsin
+            transition={{ duration: 0.8 }} // Animasyon süresi 0.6 saniye
+            className="relative w-110 h-110 mx-auto bg-[var(--primary-bg)] rounded-lg pt-5"
+          >
+            <div className="relative w-110 h-110 mx-auto bg-[var(--primary-bg)] rounded-lg pt-5">
+              {/* Profil Fotoğrafı ve Kullanıcı Bilgileri */}
+              <div
+                className="flex items-center justify-start relative w-100 h-30 bg-[var(--secondary-bg)] mx-auto pl-10 rounded-lg bg-cover bg-center"
+                style={{ backgroundImage: `url(${profileBackground2_small})` }}
+              >
+                {/* Profil Fotoğrafı Dönen Kart */}
+                <div className="relative w-22 h-22 group">
+                  <div className="w-full h-full relative transition-transform duration-700 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
+                    {/* Ön Yüz (Profil Fotoğrafı) */}
+                    <div className="absolute w-full h-full backface-hidden">
+                      <img
+                        src={pfp}
+                        alt="Profil"
+                        className="w-full h-full rounded-full border-4 border-[var(--tertiary-border)] shadow-lg"
+                      />
+                    </div>
+                    {/* Arka Yüz (Arkadaş Ekle Butonu) */}
+                    <div className="absolute w-full h-full flex items-center justify-center bg-[var(--secondary-bg)] rounded-full border-4 border-[var(--tertiary-border)] shadow-lg [transform:rotateY(180deg)] backface-hidden">
+                      <button
+                        className="text-white text-xl"
+                        onClick={handleAddFriend}
+                      >
+                        <IoMdPersonAdd />
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                {/* Kullanıcı Bilgileri */}
+                <div>
+                  <h2 className="mt-2 text-xl font-bold">
+                    {friendData?.nickName || "Bilinmeyen Kullanıcı"}
+                  </h2>
+                  <p className="text-gray-400">
+                    #{friendData?.friendshipID || "ID Bulunamadı"}
+                  </p>
+                  <span className="text-green-500 text-sm">● Çevrimiçi</span>
+                </div>
+              </div>
+              {/* Biyografi Alanı */}
+              <div className="p-4">
+                <h3 className="text-lg font-semibold">Biyografi</h3>
+                <p className="text-sm text-gray-300">
+                  Buraya kullanıcının biyografisi gelecek...
+                </p>
+              </div>
+              {/* Sunucular ve Yakın Arkadaşlar */}
+              <div className="grid grid-cols-2 gap-4 p-4">
+                {/* Sunucular */}
+                <div className="bg-[var(--secondary-bg)] p-4 rounded-lg shadow">
+                  <h3 className="font-semibold mb-2">Sunucular</h3>
+                  <hr className="border-t border-[var(--tertiary-border)] w-8/9 mx-auto mb-2" />
+                  <ul className="space-y-1 flex flex-col items-center">
+                    <li className="flex items-center gap-2 ">
+                      <img src={pfp} className="w-6 h-6 rounded-full" alt="" />{" "}
+                      Sunucu 1
+                    </li>
+                    <li className="flex items-center gap-2">
+                      <img src={pfp} className="w-6 h-6 rounded-full" alt="" />{" "}
+                      Sunucu 2
+                    </li>
+                    <li className="flex items-center gap-2">
+                      <img src={pfp} className="w-6 h-6 rounded-full" alt="" />{" "}
+                      Sunucu 3
+                    </li>
+                  </ul>
+                </div>
+                {/* Yakın Arkadaşlar */}
+                <div className="bg-[var(--secondary-bg)] p-4 rounded-lg shadow">
+                  <h3 className="font-semibold mb-2">Yakın Arkadaşlar</h3>
+                  <hr className="border-t border-[var(--tertiary-border)] w-8/9 mx-auto mb-2" />
+                  <ul className="space-y-1 flex flex-col items-center">
+                    <li className="flex items-center gap-2">
+                      <img src={pfp} className="w-6 h-6 rounded-full" alt="" />{" "}
+                      Arkadaş 1
+                    </li>
+                    <li className="flex items-center gap-2">
+                      <img src={pfp} className="w-6 h-6 rounded-full" alt="" />{" "}
+                      Arkadaş 2
+                    </li>
+                    <li className="flex items-center gap-2">
+                      <img src={pfp} className="w-6 h-6 rounded-full" alt="" />{" "}
+                      Arkadaş 3
+                    </li>
+                  </ul>
                 </div>
               </div>
             </div>
-
-            {/* Kullanıcı Bilgileri */}
-            <div>
-              <h2 className="mt-2 text-xl font-bold">
-                {friendData?.nickName || "Bilinmeyen Kullanıcı"}
-              </h2>
-              <p className="text-gray-400">
-                #{friendData?.friendshipID || "ID Bulunamadı"}
-              </p>
-              <span className="text-green-500 text-sm">● Çevrimiçi</span>
-            </div>
-          </div>
-
-          {/* Biyografi Alanı */}
-          <div className="p-4">
-            <h3 className="text-lg font-semibold">Biyografi</h3>
-            <p className="text-sm text-gray-300">
-              Buraya kullanıcının biyografisi gelecek...
-            </p>
-          </div>
-
-          {/* Sunucular ve Yakın Arkadaşlar */}
-          <div className="grid grid-cols-2 gap-4 p-4">
-            {/* Sunucular */}
-            <div className="bg-[var(--secondary-bg)] p-4 rounded-lg shadow">
-              <h3 className="font-semibold mb-2">Sunucular</h3>
-              <hr className="border-t border-[var(--tertiary-border)] w-8/9 mx-auto mb-2" />
-              <ul className="space-y-1 flex flex-col items-center">
-                <li className="flex items-center gap-2 ">
-                  <img src={pfp} className="w-6 h-6 rounded-full" alt="" />{" "}
-                  Sunucu 1
-                </li>
-                <li className="flex items-center gap-2">
-                  <img src={pfp} className="w-6 h-6 rounded-full" alt="" />{" "}
-                  Sunucu 2
-                </li>
-                <li className="flex items-center gap-2">
-                  <img src={pfp} className="w-6 h-6 rounded-full" alt="" />{" "}
-                  Sunucu 3
-                </li>
-              </ul>
-            </div>
-
-            {/* Yakın Arkadaşlar */}
-            <div className="bg-[var(--secondary-bg)] p-4 rounded-lg shadow">
-              <h3 className="font-semibold mb-2">Yakın Arkadaşlar</h3>
-              <hr className="border-t border-[var(--tertiary-border)] w-8/9 mx-auto mb-2" />
-              <ul className="space-y-1 flex flex-col items-center">
-                <li className="flex items-center gap-2">
-                  <img src={pfp} className="w-6 h-6 rounded-full" alt="" />{" "}
-                  Arkadaş 1
-                </li>
-                <li className="flex items-center gap-2">
-                  <img src={pfp} className="w-6 h-6 rounded-full" alt="" />{" "}
-                  Arkadaş 2
-                </li>
-                <li className="flex items-center gap-2">
-                  <img src={pfp} className="w-6 h-6 rounded-full" alt="" />{" "}
-                  Arkadaş 3
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
+          </motion.div>
+        )}
       </motion.div>
     </>
   );
@@ -199,6 +221,17 @@ const handleAddFriend = () => {
       message.remove(); // Mesajı DOM'dan kaldır
     }, 500); // Kaybolma animasyonunun bitmesini bekle
   }, 2000);
+};
+
+const addFriendAnim = () => {
+  return (
+    <DotLottieReact
+      src="https://lottie.host/7ae9face-ddcd-4284-8dfe-19efef04d56b/sySXGDavLA.lottie"
+      loop
+      autoplay
+      style={{ width: 300, height: 300 }}
+    />
+  );
 };
 
 export default ArkadasEkle;
