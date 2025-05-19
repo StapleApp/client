b5e97002efe46fc805d62811c82b4d8dd07f2b5f
YusufBalmumcu <yusufbalmumcu1@hotmail.com>
2025-05-19 13:08:55 +0300

    update

--- src/pages/CreateServer.jsx
+++ src/pages/CreateServer.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import toast from 'react-hot-toast';
-import { writeServerData } from "../../firebase"; // Firebase'den sunucu verilerini yazmak için fonksiyon
+
 
 
 
